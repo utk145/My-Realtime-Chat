fbfda62988281e8663b388483722cf9bf5ca70fc
utk145 <utkarsh20241a0514@grietcollege.com>
2023-09-17 17:46:38 +0530

    pre-realtime-done

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,17 +1,48 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import ChatArea from "../components/ChatArea";
 import Form from "../components/Form";
 import { authOptions } from "../lib/auth";
+import { prisma } from "../lib/db";
 
-export default async function Home(){
+
+
+async function getData() {
+    const data = await prisma.message.findMany({
+        select: {
+            message: true,
+            id: true,
+            User: {
+                select: {
+                    name: true,
+                    image: true,
+                }
+            }
+        },
+        orderBy: {
+            createdAt: 'asc'
+        },
+        take: 50,
+    });
+
+    return data;
+
+};
+
+
+export default async function Home() {
 
     const session = await getServerSession(authOptions);
-    if(!session){
+
+    const data = await getData();
+
+    if (!session) {
         redirect("/");
     }
-    return(
+    return (
         <div className="h-screen bg-gray-200 flex flex-col">
-            <Form/>                    
+            <ChatArea data={data as any}/>
+            <Form />
         </div>
     )
 }
--- app/components/ChatArea.tsx
+++ app/components/ChatArea.tsx
@@ -1,7 +1,36 @@
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
 
-export function ChatArea(){
-    return(
-        <h2>Hey</h2>
+interface ChatProp {
+    data: {
+        User: {
+            image: string | null,
+            name: string | null,
+        },
+        message: string;
+    }[];
+}
+
+export default function ChatArea({ data }: ChatProp) {
+
+    const [total, setTotal] = useState(data);
+    // console.log(total);
+    
+    return (
+        <div className="p-6 flex-grow max-h-screen py-32 overflow-y-auto">
+            <div className="flex flex-col gap-4">
+                {total.map((msg, indx) => (
+                    <div key={indx} className=''>
+                        <div className="flex items-center">
+                            <Image src={msg?.User?.image as string || "https://img.freepik.com/free-photo/fashion-boy-with-yellow-jacket-blue-pants_71767-96.jpg?size=626&ext=jpg"} alt="Profile-img" className="w-10 h-10 object-cover rounded-lg mr-4" width={50} height={50} />
+                            <div className="rounded-lg bg-[#82f3ad] p-3 shadow-md self-start">{msg.message}</div>
+                        </div>
+                        <p className="text-sm text-gray-700">{msg.User?.name as string}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
